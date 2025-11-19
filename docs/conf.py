c21270921f4401877005b90846d40bbdb7474b70
scooglat <jaanov2013@gmail.com>
2025-11-19 14:23:16 -0500

    sphinx-quickstart

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,28 @@
-# idk.
-project = "myrepairapp-api"
-author = "scooglat"
-copyright = "2025 scooglat"
-version = "0.0.0"
-release = "0.0.0"
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MyRepairApp.py'
+copyright = '2025, scooglat'
+author = 'scooglat'
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+.. MyRepairApp.py documentation master file, created by
+   sphinx-quickstart on Wed Nov 19 14:22:36 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+MyRepairApp.py documentation
+============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
